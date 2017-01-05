075da1e6e459337153dfdb46c5072dfebfccf04f
zhangweizwzw <zhangwei_7154@163.com>
2017-01-05 11:39:46 +0800

    添加EventBus

---
+++ app/src/main/java/com/example/zw/demolist/ImageCacheActivity.java
@@ -0,0 +1,15 @@
+package com.example.zw.demolist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImageCacheActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_cache);
+
+
+    }
+}
--- app/src/main/java/com/example/zw/demolist/MainActivity.java
+++ app/src/main/java/com/example/zw/demolist/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends Activity implements DemolistAdapter.OnItemClic
     private PullLoadMoreRecyclerView demolist;
     private List<DemoBean> clist=new ArrayList<DemoBean>();
     private DemolistAdapter mDemolistAdapter;
-    private String strList[]=new String[]{"微信底部导航","QQ 6.0侧滑","多级滑动","向右滑动关闭activity","ButterKnife使用","小功能","隐藏toolbar","EventBus"};
+    private String strList[]=new String[]{"微信底部导航","QQ 6.0侧滑","多级滑动","向右滑动关闭activity","ButterKnife使用","小功能","隐藏toolbar","EventBus","图片三级缓存"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,6 +82,9 @@ public class MainActivity extends Activity implements DemolistAdapter.OnItemClic
                 break;
             case 8:
                 startActivity(new Intent(this,EventBusActivity.class));
+                break;
+            case 9:
+                startActivity(new Intent(this,ImageCacheActivity.class));
                 break;
         }
     }
